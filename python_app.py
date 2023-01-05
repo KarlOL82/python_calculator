a603aaa538d42eb487d12d3a5d13c636eff03f2b
KarLOL82 <klinfeldt@gmail.com>
2023-01-04 20:39:37 -0800

    building basic calculator functions

---
+++ python_app.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    answer = a + b
+    print(str(a) + "+" str(b) + "=" + str(answer))
+
+
+def sub(a, b):
+    answer = a - b
+    print(str(a) + "-" + str(b) + "=" str(answer))
+
+
+def mul(a, b):
+    answer = a * b
+    print(str(a) + "*" + str(b) + "=" + str(answer))
+
+
+def div(a, b):
+    answer =
